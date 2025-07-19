317c76b6e59b4cf7bcb25958b5684f2535ae94f7
rusgns01 <rusgns01@naver.com>
2025-07-19 13:35:20 +0900

    Delete posts_follow directory

--- posts_follow/posts_follow/src/main/java/com/example/crud/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.entity.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
-}
